bae769c2e855221496e668835747a7ad83224060
chevychelovN <86499004+chevychelovN@users.noreply.github.com>
2022-09-10 20:45:16 +0300

    Added task with bash script
    Напишите скрипт на bash, который принимает на вход один аргумент (целое число от 0 до бесконечности), который будет обозначать число студентов в аудитории. В зависимости от значения числа нужно вывести разные сообщения. 
    
    Соответствие входа и выхода должно быть таким:
    0 -->  No students
    1 -->  1 student
    2 -->  2 students
    3 -->  3 students
    4 -->  4 students
    5 и больше --> A lot of students

---
+++ students.sh
@@ -0,0 +1,13 @@
+val1=$1
+if [[ $1 -eq 0 ]]
+then 
+echo "No students"
+elif [[ $1 -eq 1 ]]
+then
+echo "$1 student"
+elif [[ $1 -ge 2 && $1 -le 4 ]]
+then
+echo "$1 students"
+else
+echo "A lot of students"
+fi
